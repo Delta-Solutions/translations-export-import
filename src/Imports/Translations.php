3b4b42518eb97f913b62b5fab85bf7cc220391c2
Dieter Coopman <dieter@deltasolutions.be>
2023-04-26 21:42:04 +0200

    add comments
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- src/Imports/Translations.php
+++ src/Imports/Translations.php
@@ -14,22 +14,30 @@ class Translations implements ToModel, WithProgressBar, WithHeadingRow
 {
     use Importable;
 
-    public function model(array $row)
+    public function model(array $row): ?LanguageLine
     {
+        //remove the id from the row
         unset($row['id']);
+        //set the text field to an empty array
         $row['text'] = [];
 
-        $fields = Schema::getColumnListing('language_lines');
+        //get the difference between the columns in the database and the columns in the excel file
+        $fields     = Schema::getColumnListing('language_lines');
         $textFields = array_diff(array_keys($row), $fields);
+
+        //only get the text fields from the row
         $valuesToExtract = array_intersect_key($row, array_flip($textFields));
+        //filter out empty values
         $filteredArray = array_filter($valuesToExtract, function ($value) {
-            return ! blank($value);
+            return !blank($value);
         });
-        $row['text'] = $filteredArray;
+        //set the text field to the filtered array
+        $row['text']       = $filteredArray;
         $row['updated_at'] = Carbon::now();
+        //remove the text fields from the row
         $row = array_diff_key($row, array_flip($textFields));
         if (filled($row['group']) && filled($row['key'])) {
-        return LanguageLine::updateOrCreate(['group' => $row['group'], 'key' => $row['key']], $row);
-    }
+            return LanguageLine::updateOrCreate(['group' => $row['group'], 'key' => $row['key']], $row);
+        }
     }
 }
